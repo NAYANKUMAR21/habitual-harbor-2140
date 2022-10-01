f020c902161bf8c606443ce68865cfab1878cdd6
Nayan Kumar <naayaankumar@gmail.com>
2022-10-01 20:59:28 +0530

    done whole project

--- ayoa_clone/src/components/Allroutes/Allroutes.jsx
+++ ayoa_clone/src/components/Allroutes/Allroutes.jsx
@@ -3,13 +3,14 @@ import {Routes,Route} from "react-router-dom"
 import Dashboard from '../Pages/Dashboard/Dashboard'
 import Mainbody from '../Pages/home/Mainbody'
 import Login from '../Pages/LoggedIn/Login'
+import PrivateRoute from './PrivateRoute'
 const Allroutes = () => {
   return (
     <>
     <Routes>
         <Route path="/" element={<Mainbody/>}></Route>
         <Route path="/login" exact element={<Login />}></Route>
-        <Route path="/login/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/login/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>}></Route>
     </Routes>
     </>
   )
--- ayoa_clone/src/components/Context/AuthContext.jsx
+++ ayoa_clone/src/components/Context/AuthContext.jsx
@@ -6,12 +6,14 @@ export const AuthContext = createContext()
 function AuthContextProvider({children}){
     const [Auth,setAuth] = useState(false)
     const [token,setToken] = useState({})
+    const [name,setName] = useState({})
     const handleAuth  = ()=>{
         setAuth(!Auth)
     }
     console.log(token,"auth")
+    
     return(
-        <AuthContext.Provider value={{Auth,handleAuth,setToken,token}}>
+        <AuthContext.Provider value={{Auth,handleAuth,setToken,token,setName,name}}>
             {children}
         </AuthContext.Provider>
     ) 
